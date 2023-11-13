e331c7c42a38675abd40bbfb35392a1b9d7f6ad4
maricd7 <87302839+maricd7@users.noreply.github.com>
2023-11-13 08:15:26 +0100

    Add files via upload

--- toast-notifications/app.js
+++ toast-notifications/app.js
@@ -1,33 +1,99 @@
-let toastContainer = document.getElementById('container');
+let notificationsContainer = document.getElementById("notifications");
 
 let succcessIcon = "fluent-emoji:check-mark-button";
-let alertIcon = '' ; 
-let failIcon  = ''; 
+let alertIcon = "fluent-emoji:warning";
+let failIcon = "fluent-emoji:double-exclamation-mark";
 let closeIcon = "material-symbols:close-rounded";
 
-let close = document.getElementById("close-icon");
-let succcessIconConatainer = document.getElementById("scs-icon");
+let successText = "Your request was a success.";
+let alertText = "This is a alerting text";
+let failText = "Your request failed.";
 
 //buttons
-let successBtn = document.getElementById('success-btn'); 
-let failBtn = document.getElementById('fail-btn')
-let alertBtn = document.getElementById('alert-btn');
+let successBtn = document.getElementById("success-btn");
+let failBtn = document.getElementById("fail-btn");
+let alertBtn = document.getElementById("alert-btn");
 
+//buttons functions
 
-//appending
+successBtn.addEventListener("click", () => {
+  let toastContainer = document.createElement("div");
+  toastContainer.classList.add("toast-notification");
+  let close = document.createElement("iconify-icon");
+  close.icon = closeIcon;
+  let text = document.createElement("p");
+  let iconContainer = document.createElement("iconify-icon");
+  iconContainer.icon = succcessIcon;
 
+  text.innerText = successText;
 
-successBtn.addEventListener('click', ()=>{
-    toastContainer.style.display='block'; 
-    succcessIconConatainer.icon = succcessIcon;
-    close.icon = closeIcon; 
+  toastContainer.appendChild(iconContainer);
+  toastContainer.appendChild(text);
+  toastContainer.appendChild(close);
 
-})
-failBtn.addEventListener('click', ()=>{
+  notificationsContainer.appendChild(toastContainer);
 
-})
-alertBtn.addEventListener('click', ()=>{
+  close.icon = closeIcon;
 
-})
+  // closing logic
+  close.addEventListener("click", () => {
+    toastContainer.remove();
+  });
+  setTimeout(() => {
+    toastContainer.remove();
+  }, 3000);
+});
+failBtn.addEventListener("click", () => {
+  let toastContainer = document.createElement("div");
+  toastContainer.classList.add("toast-notification");
+  let close = document.createElement("iconify-icon");
+  close.icon = closeIcon;
+  let text = document.createElement("p");
+  let iconContainer = document.createElement("iconify-icon");
+  iconContainer.icon = failIcon;
 
+  text.innerText = failText;
 
+  toastContainer.appendChild(iconContainer);
+  toastContainer.appendChild(text);
+  toastContainer.appendChild(close);
+
+  notificationsContainer.appendChild(toastContainer);
+
+  close.icon = closeIcon;
+
+  // closing logic
+  close.addEventListener("click", () => {
+    toastContainer.remove();
+  });
+  setTimeout(() => {
+    toastContainer.remove();
+  }, 3000);
+});
+alertBtn.addEventListener("click", () => {
+    let toastContainer = document.createElement("div");
+    toastContainer.classList.add("toast-notification");
+    let close = document.createElement("iconify-icon");
+    close.icon = closeIcon;
+    let text = document.createElement("p");
+    let iconContainer = document.createElement("iconify-icon");
+    iconContainer.icon = alertIcon;
+  
+    text.innerText = alertText;
+  
+    toastContainer.appendChild(iconContainer);
+    toastContainer.appendChild(text);
+    toastContainer.appendChild(close);
+  
+    notificationsContainer.appendChild(toastContainer);
+  
+    close.icon = closeIcon;
+  
+    // closing logic
+    close.addEventListener("click", () => {
+      toastContainer.remove();
+    });
+    setTimeout(() => {
+      toastContainer.remove();
+    }, 3000);
+});
