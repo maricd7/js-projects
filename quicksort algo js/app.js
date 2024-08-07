2cdc9fdd0b79754f92b1c70b0eb874fe70fd5de8
maricd7 <87302839+maricd7@users.noreply.github.com>
2024-08-07 22:41:37 +0200

    quicksort algo js

---
+++ quicksort algo js/app.js
@@ -0,0 +1,25 @@
+const quicksort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) continue;
+
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quicksort(left), pivot, ...quicksort(right)];
+};
+
+const array = [3, 6, 8, 10, 1, 2, 1];
+const sortedArray = quicksort(array);
+console.log(sortedArray);
